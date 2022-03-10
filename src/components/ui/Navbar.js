306ca4ee15ee492571289e74addfd0ea9aa98c5e
Alexis Castellanos <castellanos.alexis@hotmail.com>
2022-03-10 13:13:35 -0600

    added useNavigate

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
+
+	const navigate = useNavigate();
+
 	const handleLogout = () => {
-		//TODO: Por Hacer
+
 		console.log("logout");
+		navigate('/login', {
+			replace: true
+		});
 	};
 	return (
 		<nav className="navbar navbar-expand-sm navbar-dark bg-dark">
@@ -37,7 +43,10 @@ export const Navbar = () => {
 			<div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
 				<ul className="navbar-nav ml-auto">
 					<span className="nav-item nav-link text-info">Alexis</span>
-					<button className="nav-item nav-link btn" onClick={handleLogout}>
+					<button
+						className="nav-item nav-link btn"
+						onClick={handleLogout}
+					>
 						Logout
 					</button>
 				</ul>
