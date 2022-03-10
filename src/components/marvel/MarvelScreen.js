70dcd0506045a01e0d52f6e4842f2da7083e9cfb
Alexis Castellanos <castellanos.alexis@hotmail.com>
2022-03-10 14:19:06 -0600

    show list of heroes

--- src/components/marvel/MarvelScreen.js
+++ src/components/marvel/MarvelScreen.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { HeroList } from '../hero/HeroList';
 
 export const MarvelScreen = () => {
   return (
-    <div>MarvelScreen</div>
+    <>
+      <h1>MarvelScreen</h1>
+      <hr />
+      <HeroList publisher='Marvel Comics' />
+    </>
   )
 }
