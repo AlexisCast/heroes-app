9927267d2bd96345368d4517d125f1ceb69fd2c1
Alexis Castellanos <castellanos.alexis@hotmail.com>
2022-03-11 13:09:39 -0600

    implement useNavigate and Navigate reading arguments from URL

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { useParams, Navigate } from 'react-router-dom';
+import { getHeroById } from '../../selectors/getHeroById';
+
+
 
 export const HeroScreen = () => {
+  const { heroeId } = useParams();
+  // console.log(heroeId);
+  const hero = getHeroById(heroeId);
+  console.log(hero);
+  if (!hero) {
+    return <Navigate to='/' />
+  }
   return (
-    <div>HeroScreen</div>
+    <div>
+      <h1>Hero Screen</h1>
+      <p>{hero.superhero}</p>
+    </div>
   )
 }
