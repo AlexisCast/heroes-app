fbf71e28d3fad8a20fb052697262e4f9293855f4
Alexis Castellanos <castellanos.alexis@hotmail.com>
2022-03-11 14:04:53 -0600

    implement useMemo in HeroList

--- src/components/hero/HeroList.js
+++ src/components/hero/HeroList.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import { HeroCard } from './HeroCard';
 import { getHeroesByPublisher } from '../../selectors/getHeroesByPublisher'
 
 export const HeroList = ({ publisher }) => {
-   const heroes = getHeroesByPublisher(publisher);
 
+   // const [counter, setCounter] = useState(0);
+
+   const heroes =useMemo(()=>getHeroesByPublisher(publisher),[publisher]);
+   // const heroes= getHeroesByPublisher(publisher);
+
+   // const handleCounter = () => {
+   //    setCounter(counter + 1);
+   //  }
    return (
       <div
          className='row rows-cols-1 row-cols-md-3 g-3'
@@ -18,7 +25,13 @@ export const HeroList = ({ publisher }) => {
                />
             ))
          }
-
+         {/* {counter}
+         <button
+          className='btn btn-outline-info'
+          onClick={handleCounter}
+        >
+          Return 
+        </button> */}
       </div>
    )
 }
