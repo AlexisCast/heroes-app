51b0d9b282ef1ab2964a139dd86a55597bf8f90c
Alexis Castellanos <castellanos.alexis@hotmail.com>
2022-03-12 03:56:49 -0600

    implement login function using localstorage

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../auth/authContext";
 
 export const LoginScreen = () => {
+	const { dispatch } = useContext(AuthContext);
+
 	const navigate = useNavigate();
 
 	const handleLogin = () => {
-		// console.log("handleclick");//
+		const action = {
+			type: '[auth] Login',
+			payload: {
+				name: 'Alexis'
+			}
+		}
+
+		dispatch(action)
+		
 		navigate('/marvel', {
 			replace: true
 		});
